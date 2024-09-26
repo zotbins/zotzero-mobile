78a16295fd69d4020ab585c1295ea2a4aac1951b
Evan Servito <78441979+evanpservito@users.noreply.github.com>
2024-09-25 18:53:15 -0700

    add maps feature and sample markers

---
+++ assets/markers.js
@@ -0,0 +1,26 @@
+export const markers = [
+  // Science Library
+  {
+    latitude: 33.6458403181145,
+    longitude: -117.84649584653289,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Science Library",
+  },
+  // Langson Library
+  {
+    latitude: 33.6472545668769,
+    longitude: -117.8412069560408,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Langson Library",
+  },
+  // Student Center
+  {
+    latitude: 33.649268327033056,
+    longitude: -117.84263466182622,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    name: "Student Center",
+  },
+];
