2115c5053e913fefee8d2e37666382ae63783880
EthanSantos <ethanangelo61@gmail.com>
2024-11-18 13:28:28 -0800

    Added button to go to camera screen

--- app/(auth)/(tabs)/scan.tsx
+++ app/(auth)/(tabs)/scan.tsx
@@ -1,14 +1,43 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Stack } from "expo-router";
+import { Camera, useCameraDevice } from "react-native-vision-camera";
 
 const Scan = () => {
+  const [cameraVisible, setCameraVisible] = useState(false);
+  const device = useCameraDevice('back');
+
+  const requestCameraPermission = async () => {
+    const permission = await Camera.requestCameraPermission();
+    if (permission === 'granted') {
+      setCameraVisible(true);
+    } else {
+      alert("Camera permission is required to scan");
+    }
+  };
+
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={styles.container}>
-        <Text style={styles.text}>Scan</Text>
+        {!cameraVisible ? (
+          <>
+            <TouchableOpacity style={styles.button} onPress={requestCameraPermission}>
+              <Text style={styles.buttonText}>Scan Food Waste</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          device != null ? (
+            <Camera
+              style={StyleSheet.absoluteFill}
+              device={device}
+              isActive={cameraVisible}
+            />
+          ) : (
+            <Text style={styles.text}>No physical camera detected.</Text>
+          )
+        )}
       </View>
     </>
   );
@@ -25,5 +54,15 @@ const styles = StyleSheet.create({
   },
   text: {
     color: Colors.black,
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
   },
 });
