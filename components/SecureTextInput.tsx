9bd021a8934bfdc5df830c21ab6ed9b03081e5f4
cyrusbuffington <cyrusbuffington@gmail.com>
2024-11-25 17:14:33 -0800

    secure password requirement

---
+++ components/SecureTextInput.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+interface SecureTextInputProps {
+    password: string;
+    setPassword: (text: string) => void;
+    placeholder: string;
+    className?: any;
+}
+
+const SecureTextInput: React.FC<SecureTextInputProps> = ({ password, setPassword, placeholder, className }) => {
+  const [secureTextEntry, setSecureTextEntry] = useState(true);
+
+  return (
+    <View className="relative w-72">
+      <TextInput
+        className={className}
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity
+        className="absolute right-0 top-0 h-full justify-center pr-3"
+        onPress={() => setSecureTextEntry(!secureTextEntry)}
+      >
+        <Ionicons name={secureTextEntry ? "eye-off" : "eye"} size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SecureTextInput;
